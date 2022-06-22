4c2c78e3f7a02caff960065ef7729caa09f1655e
Carlos Vicente <carlos.quevedo.1994@gmail.com>
2022-06-21 18:09:27 -0600

    Primer commit del proyecto

---
+++ Excersice.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utg-8 -*-
+
+__author__ = "Carlos Vicente"
+__credits__ = ["Carlos Vicente"]
+__Lisence__ = "BSD"
+__maintainer__ = "Carlos Vicente"
+__email__ = "carlos.quevedo.1994@gmail.com"
+__status__ = "Development"
+__version__ = "0.0.1"
+
+# Default python packages
+import logging
+import os
